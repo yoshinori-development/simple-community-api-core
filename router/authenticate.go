e5b7c4642a8a0548d2b6b8246a4e54fb36737c13
yoshinori satoh <yoshinori.satoh.tokyo@gmail.com>
2021-10-03 14:46:35 +0000

    up

--- router/authenticate.go
+++ router/authenticate.go
@@ -49,7 +49,7 @@ func authenticate(awsConf config.Aws) gin.HandlerFunc {
 		log.Print(h)
 		if h.Data != "" {
 			fmt.Println(h.Data)
-			tokenString := strings.Replace(h.Data, "=", "", -1)
+			tokenString := strings.Replace(h.Data, "=", "", 2)
 			token, err := jwt.Parse(tokenString, func(tk *jwt.Token) (interface{}, error) {
 				fmt.Println(tk)
 				fmt.Println("aaaaaaaaaaaaaa")
